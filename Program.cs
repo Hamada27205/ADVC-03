c4fb70623466351ea708b768609e44bb7f184c0a
Hamada27205 <ahmedfarag8273@gmail.com>
2026-09-05 02:57:00 +0300

    Exercise 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Exercise 1
+
+            //List<int> grades = [85, 92, 78, 95, 88, 70, 100, 65];
+
+            //foreach (int grade in grades) Console.WriteLine(grade);
+            //Console.WriteLine("===================================");
+            //Console.WriteLine("Count");
+            //Console.WriteLine(grades.Count);
+            //Console.WriteLine("===================================");
+            //Console.WriteLine("The First And The Last");
+            //Console.WriteLine(grades[0]);
+            //Console.WriteLine(grades[7]);
+            //Console.WriteLine("===================================");
+            //Console.WriteLine("Sort");
+            //grades.Sort();
+            //foreach (int grade in grades) Console.WriteLine(grade);
+            //Console.WriteLine("===================================");
+            //Console.WriteLine("the first grade above 90");
+            //int First = grades.Find(N => N > 90);
+            //Console.WriteLine(First);
+            //Console.WriteLine("===================================");
+            //Console.WriteLine("Get all grades below 75 (failing grades)");
+            //List<int> failingGrades = grades.FindAll(N => N < 75);
+            //foreach (int grade in failingGrades) Console.WriteLine(grade);
+
+            //Console.WriteLine("===================================");
+            //Console.WriteLine("Remove all failing grades (below 75)");
+            //grades.RemoveAll(N => N < 75);
+            //foreach (int grade in grades) Console.WriteLine(grade);
+            //Console.WriteLine("===================================");
+            //Console.WriteLine("Check if any grade equals 100");
+            //bool Hundred = grades.Any(N => N == 100);
+            //Console.WriteLine(Hundred);
+            //Console.WriteLine("===================================");
+            //Console.WriteLine("Create a List<string> where each grade becomes Grade: X");
+            //List<string> Grades = grades.Select(N =>  $"Grade: {N}").ToList();
+            //foreach (string grade in Grades) Console.WriteLine(grade);
+
+            #endregion
+
         }
     }
 }
